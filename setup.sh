317ce1d90f6ffd2e166aadf8529494b64642b5d0
renji <renjiseminar@gmail.com>
2017-01-30 16:52:49 +0900

    setup.sh 更新.

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-rm ./dist/*
+rm -r ./dist/*
+touch ./dist/.gitkeep
 cp -r ./bower_components/bootstrap/dist ./dist/bootstrap
 cp -r ./bower_components/jquery-ui/themes/smoothness ./dist/jquery-ui
 cp ./bower_components/jquery/dist/jquery.min.js ./dist/
