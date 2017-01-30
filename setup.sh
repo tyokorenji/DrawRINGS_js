f16b6da2dfa1a341abbc9f79f21e55dd9db28bb8
renji <renjiseminar@gmail.com>
2017-01-30 16:48:36 +0900

    dist ディレクトリ周りのプロジェクト構造の変更.

---
+++ setup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+rm ./dist/*
+cp -r ./bower_components/bootstrap/dist ./dist/bootstrap
+cp -r ./bower_components/jquery-ui/themes/smoothness ./dist/jquery-ui
+cp ./bower_components/jquery/dist/jquery.min.js ./dist/
+
+cp ./index.html ./dist/
+cp ./style.css ./dist/
+
